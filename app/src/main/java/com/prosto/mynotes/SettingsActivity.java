21ee8393b373c581c3bda8e0f14faa1362ebb2f0
XBICT <rosstyk@gmail.com>
2015-09-08 16:53:43 +0300

    v0.7.5 settings Files(not ready) cardView navDrawer toolbar noteAction

---
+++ app/src/main/java/com/prosto/mynotes/SettingsActivity.java
@@ -0,0 +1,47 @@
+package com.prosto.mynotes;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.view.View;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    public Toolbar toolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.settings_layout);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+
+        initToolbar();
+    }
+
+    private void initToolbar(){
+        toolbar.findViewById(R.id.toolbar);
+
+        if (toolbar!= null){
+            setSupportActionBar(toolbar);
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            getSupportActionBar().setHomeButtonEnabled(true);
+        }
+        toolbar.setNavigationIcon(R.drawable.keyboard_backspace);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
+
+        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem menuItem) {
+                return false;
+            }
+        });
+
+    }
+
+}
